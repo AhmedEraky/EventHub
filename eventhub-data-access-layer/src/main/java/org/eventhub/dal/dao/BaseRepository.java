d576f70246922157d4639cfc8ada10420fdccc1f
Aya Taha <aya.taha.ali.93@gmail.com>
2019-05-22 17:21:45 +0200

    modifing in daos comments

--- eventhub-data-access-layer/src/main/java/org/eventhub/dal/dao/BaseRepository.java
+++ eventhub-data-access-layer/src/main/java/org/eventhub/dal/dao/BaseRepository.java
@@ -28,7 +28,7 @@ public interface BaseRepository<T extends BaseEntity> extends JpaRepository<T, U
     /**
      *  soft delete an object
      * @param uuid the id of the entity
-     * @author Aya Taha
+     * @author Aya Taha (aya.taha.ali.93@gmail.com)
      */
     @Query("update #{#entityName} e set e.deleted=true where e.uuid=?1")
     @Modifying
@@ -37,7 +37,8 @@ public interface BaseRepository<T extends BaseEntity> extends JpaRepository<T, U
 
     /**
      *  soft delete an object
-     * @author Ahmed Eraky <ahmedmoeraky@gmail.com>
+     * @author Ahmed Eraky (ahmedmoeraky@gmail.com)
+     * @param t the domain type the repository manages
      */
     @Query("update #{#entityName} e set e.deleted=true where e=?1")
     @Modifying
@@ -46,7 +47,9 @@ public interface BaseRepository<T extends BaseEntity> extends JpaRepository<T, U
 
     /**
      *  soft delete an object by ID
-     * @author Ahmed Eraky <ahmedmoeraky@gmail.com>
+     * @param uuid the id of the entity
+     * @author Ahmed Eraky (ahmedmoeraky@gmail.com)
+     * 
      */
     @Query("update #{#entityName} e set e.deleted=true where e.uuid=?1")
     @Modifying
@@ -56,44 +59,83 @@ public interface BaseRepository<T extends BaseEntity> extends JpaRepository<T, U
 
     /**
      *  retrieve the existing entities
+     * @param pageable Set of pages
      * @return list of entity
-     * @author Aya Taha
+     * @author Aya Taha (aya.taha.ali.93@gmail.com)
      */
     @Query("from #{#entityName} e where e.deleted=0")
+    @Override
     public Page<T> findAll(Pageable pageable);
 
 
 
     /**
-     *  retrieve deleted entities
+     *  retrieve all deleted entities
+     * @param pageable Set of pages
      * @return list of deleted entities
-     * @author Aya Taha
+     * @author Aya Taha (aya.taha.ali.93@gmail.com)
      */
     @Query("from #{#entityName} e where e.deleted=1")
     public List<T> findAllDeleted(Pageable pageable);
-
+    /**
+     * retrieve deleted and undeleted entities
+     * @param <S> entity type
+     * @param exmpl Example
+     * @param sort Sorting data
+     * @return List of entity 
+     * @Author Aya Taha (aya.taha.ali.93@gmail.com)
+     */
     @Override
     @Query("from #{#entityName} e where e.deleted=0")
     public <S extends T> List<S> findAll(Example<S> exmpl, Sort sort);
 
+    /**
+     * retrieve deleted and undeleted entities
+     @param <S> entity type
+     * @param exmpl Example
+     * @return List of entity 
+     * @Author Aya Taha (aya.taha.ali.93@gmail.com)
+     */
     @Override
     @Query("from #{#entityName} e where e.deleted=0")
     public <S extends T> Page<S> findAll(Example<S> exmpl, Pageable pageable);
 
+    /**
+     * retrieve undeleted  entities by iterable of id
+     * @param itrbl Iterable of UUID
+     * @return list of entity type
+     * @Author Aya Taha (aya.taha.ali.93@gmail.com)
+     */
     @Override
     @Query("select e from #{#entityName} e where e.uuid in ?1 and e.deleted = 0")
     public List<T> findAllById(Iterable<UUID> itrbl);
 
+    /**
+     *  retrieve undeleted by Sort
+     * @param sort Sorting data
+     * @return  list of entity type
+     * @Author Aya Taha (aya.taha.ali.93@gmail.com)
+     */
     @Override
     @Query("from #{#entityName} e where e.deleted=0")
     public List<T> findAll(Sort sort);
 
-
+    /**
+     * retrieve entity by id
+     * @param id the id of entity 
+     * @return entity  entity type
+     * @Author Aya Taha (aya.taha.ali.93@gmail.com)
+     */
 
     @Override
     @Query("from #{#entityName} e where  e.uuid=?1 and e.deleted=0")
     public T getOne(UUID id);
 
+    /**
+     * update entity
+     * @param entity Entity type
+     * @Author Aya Taha (aya.taha.ali.93@gmail.com)
+     */
     default public void update(T entity){
         save(entity);
     }
