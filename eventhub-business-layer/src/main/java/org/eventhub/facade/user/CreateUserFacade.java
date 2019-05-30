26f2a2190f944dc3c9a57712e4448406944e18b1
fares <fares.helmy93@gmail.com>
2019-05-30 22:58:44 +0200

    Confirmed that multipart filter is working fine

--- eventhub-business-layer/src/main/java/org/eventhub/facade/user/CreateUserFacade.java
+++ eventhub-business-layer/src/main/java/org/eventhub/facade/user/CreateUserFacade.java
@@ -5,4 +5,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface CreateUserFacade {
     SystemUser createUser(SystemUser systemUser,  MultipartFile imageResource);
+
+    public void UpdateUser(SystemUser user);
 }
--- eventhub-service-layer/src/main/java/org/eventhub/service/user/CreateUserService.java
+++ eventhub-service-layer/src/main/java/org/eventhub/service/user/CreateUserService.java
@@ -6,4 +6,6 @@ import org.springframework.web.multipart.MultipartFile;
 public interface CreateUserService {
 
     SystemUser createSystemUser(SystemUser systemUser, MultipartFile imageResource);
+
+    public void updateUser(SystemUser systemUser);
 }
