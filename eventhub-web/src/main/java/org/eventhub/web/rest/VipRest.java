a2bac2e27dbddd325559bf4b9e1d0da9832bbfde
fares <fares.helmy93@gmail.com>
2019-06-16 22:43:11 +0200

    modifications in VipRest concerning layering

--- eventhub-web/src/main/java/org/eventhub/web/rest/VipRest.java
+++ eventhub-web/src/main/java/org/eventhub/web/rest/VipRest.java
@@ -8,8 +8,11 @@ package org.eventhub.web.rest;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.UUID;
 import org.eventhub.common.model.entity.Vip;
 import org.eventhub.dal.dao.VipRepository;
+import org.eventhub.facade.vip.CreateVipFacade;
+import org.eventhub.facade.vip.RetrieveVipFacade;
 import org.eventhub.remote.adapter.VipAdapter;
 import org.eventhub.remote.dto.JResponse;
 import org.eventhub.remote.dto.VipDTO;
@@ -18,6 +21,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -28,14 +32,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class VipRest {
 
     @Autowired
-    VipRepository vipRepository;
+    CreateVipFacade createVipFacade;
+    @Autowired
+    RetrieveVipFacade retrieveVipFacade;
     @Autowired
     VipAdapter vipAdapter;
 
     @GetMapping(name = "/vips")
     public JResponse getAllVips() {
         List<VipDTO> vipList = new ArrayList<>();
-        Iterator<Vip> vipsIterator = vipRepository.findAll().iterator();
+        Iterator<Vip> vipsIterator = retrieveVipFacade.getAll().iterator();
         vipsIterator.forEachRemaining(vip -> {
             VipDTO vipDTO = vipAdapter.toVipDTO(vip);
             vipList.add(vipDTO);
@@ -45,19 +51,27 @@ public class VipRest {
         return vipsResponse;
     }
 
+    @GetMapping(name = "/vips")
+    public JResponse getVipById(@RequestParam UUID uuid) {
+        Vip vip = retrieveVipFacade.getVipById(uuid);
+        JResponse jResponse = new JResponse();
+        jResponse.setDtoContent(vipAdapter.toVipDTO(vip));
+        return jResponse;
+    }
+
     @PostMapping(name = "/vips")
     public JResponse insertVip(@RequestBody VipDTO vipDTO) {
         Vip vip = vipAdapter.fromVipDTO(vipDTO);
-        Vip persistentVip = vipRepository.save(vip);
+        Vip persistentVip = createVipFacade.createVip(vip);
         JResponse vipResponse = new JResponse();
         vipResponse.setDtoContent(vipAdapter.toVipDTO(persistentVip));
         return vipResponse;
     }
-    
+
     @PutMapping(name = "/vips")
     public JResponse updateVip(@RequestBody VipDTO vipDTO) {
         Vip vip = vipAdapter.fromVipDTO(vipDTO);
-        vipRepository.update(vip);
+        createVipFacade.updateVip(vip);
         JResponse vipResponse = new JResponse();
         vipResponse.setDtoContent(vipAdapter.toVipDTO(vip));
         return vipResponse;
