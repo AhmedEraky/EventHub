a1bd827c7043858a308f62d1d7341bb50b4ef047
Elhosany <mohamed.elhosany1995@gmail.com>
2019-06-17 12:25:13 +0200

    adding service of adding new session

--- eventhub-web/src/main/java/org/eventhub/web/rest/remote/SessionRest.java
+++ eventhub-web/src/main/java/org/eventhub/web/rest/remote/SessionRest.java
@@ -6,7 +6,15 @@
 package org.eventhub.web.rest.remote;
 
 import java.util.List;
+
+import org.eventhub.common.model.entity.Session;
+import org.eventhub.facade.session.SessionManagementFacade;
+import org.eventhub.web.rest.remote.adapter.SessionAdapter;
+import org.eventhub.web.rest.remote.dto.JResponse;
 import org.eventhub.web.rest.remote.dto.SessionDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,19 +24,44 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api")
-public class SessionRest {
-    
+public class SessionRest
+{
+    @Autowired
+    SessionAdapter sessionAdapter;
+
+    @Autowired
+    SessionManagementFacade sessionManagementFacade;
+
     //Amr
     
-    public List<SessionDTO> getAllSession(){
-        
-    }
+
     
     //menna
     
     
     //husseny
-    
-    
+
+    /**
+     * Responsible for creating new session
+     * @author Elhosany <mohamed.elhosany1995@gmail.com>
+     * @param sessionDTO the sessionDTO that represent the session
+     * @return the sessionResponse to client
+     */
+    @PostMapping("/addSession")
+    public JResponse<SessionDTO> addSession(@RequestBody SessionDTO sessionDTO)
+    {
+        JResponse<SessionDTO> sessionResponse = new JResponse<>();
+        Session session = sessionAdapter.fromSessionDTO(sessionDTO);
+        try {
+            sessionManagementFacade.createSession(session);
+            sessionResponse.setDtoContent(sessionDTO);
+            sessionResponse.setStatus("Success");
+        }catch (Exception e){
+             sessionResponse.setStatus("Fail");
+             sessionResponse.setDtoContent(new SessionDTO());
+        }
+
+        return sessionResponse;
+    }
     
 }
